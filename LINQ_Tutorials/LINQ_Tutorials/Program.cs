6a75ee1fc13abd4942ec5e976df2803c5758118b
Tri Phan <phanphutri@gmail.com>
2022-05-10 13:55:12 +0700

    Updated

---
+++ LINQ_Tutorials/LINQ_Tutorials/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Tutorials
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // The following example shows the complete query operation.
+            // The complete operation includes creating a data source,
+            // defining a query expression, and executing the query in
+            // a freach statement.
+            
+            // Specify the data source
+            int[] scores = new int[] { 97, 92, 60, 81};
+
+            // Define the query expression
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Execute the query
+            foreach (var item in scoreQuery)
+            {
+                Console.Write($"{item} ");
+            }
+
+            // -> Output: 97 92 81
+        }
+    }
+}
