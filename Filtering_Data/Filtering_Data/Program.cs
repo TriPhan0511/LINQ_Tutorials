20ad5b867718fbc635cf96e4d3c2bf73f6db233b
Tri Phan <phanphutri@gmail.com>
2022-05-31 15:46:46 +0700

    updated

---
+++ Filtering_Data/Filtering_Data/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections;
+
+namespace Filtering_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Filtering refers to the operation of restricting the result set to contain only those elements
+                that satisfy a specified condition. It is also known as selection.
+
+                Link: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/filtering-data
+             */
+
+            //// The following example uses the where clause to filter from an array those strings that have a specific length.
+            //string[] words = { "the", "quick", "brown", "fox", "jumps" };
+
+            ////// Query syntax
+            ////var query = 
+            ////    from word in words
+            ////    where word.Length == 3
+            ////    select word;
+
+            //// Method syntax
+            //var query =
+            //    words.Where(word => word.Length == 3);
+
+            //foreach (var item in query)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine();
+
+            //// Output: the fox
+
+            // Another example: 
+            // The following example shows that the standard query operators such as
+            // Where() can be applied to the ArrayList type after calling OfType()
+            // Link: https://docs.microsoft.com/en-us/dotnet/api/system.linq.enumerable.oftype?view=net-6.0
+
+            ArrayList fruits = new ArrayList(4);
+            fruits.Add("Mango");
+            fruits.Add("Orange");
+            fruits.Add("Apple");
+            fruits.Add(3.0);
+            fruits.Add("Banana");
+
+            var query =
+                fruits.OfType<string>();
+
+            foreach (var item in query)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            // Output: Mango Orange Apple Banana
+
+            var query2 =
+                fruits.OfType<string>().Where(fruit => fruit.ToLower().Contains("n"));
+
+            foreach (var item in query2)
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+            // Output: Mango Orange Banana
+        }
+    }
+}
---
+++ Sorting_Data_In_A_Collection/Sorting_Data_In_A_Collection/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Data_In_A_Collection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                A sorting operation orders the elements of a sequence based on one or more attributes.
+                The first sort criterion performs a primary sort on the elements.
+                By specifying a second sort criterion, you can sort the elements within each primary sort group.
+             */
+
+            /*
+                I. Primary Sort Examples
+             */
+
+            /*
+                1.Primary Ascending Sort
+             */
+
+            //// The following example demonstrates how to use the orderby clause in a LINQ query
+            //// to sort the strings in an array by string length, in ascending order.
+            //string[] words = { "the", "quick", "brown", "fox", "jumps" };
+
+            //// Query syntax
+            //IEnumerable<string> query =
+            //    from word in words
+            //    orderby word.Length
+            //    select word;
+
+            ////// Method syntax
+            ////IEnumerable<string> query =
+            ////    words.OrderBy(x => x.Length);
+
+            //foreach (var word in query)
+            //{
+            //    Console.Write($"{word} ");
+            //}
+            //Console.WriteLine();
+            //// output: the fox quick brown jumps
+
+            /*
+                2.Primary Descending Sort
+             */
+
+            //string[] words = { "the", "quick", "brown", "fox", "jumps" };
+
+            //IEnumerable<string> query =
+            //    from word in words
+            //    orderby word.Length descending
+            //    select word;
+
+            ////IEnumerable<string> query =
+            ////    words.OrderByDescending(x => x.Length);
+
+            //foreach (var item in query)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //// Output: quick brown jumps the fox
+            ///
+
+            /*
+                II. Secondary Sort Examples
+             */
+
+            /*
+                1. Secondary Ascending Sort
+             */
+
+            //// The follwing example demonstrates how to use the orderby clause in a LINQ query
+            //// to perform a primary and secondary sort of the strings in an array.
+            //// The strings are sorted primarily by length and secondarily by the first letter of
+            //// the string, both in ascending order.
+
+            //string[] words = { "the", "quick", "brown", "fox", "jumps" };
+
+            //var query =
+            //    from word in words
+            //    orderby word.Length, word.Substring(0, 1)
+            //    select word;
+
+            ////var query =
+            ////    words.OrderBy(x => x.Length).ThenBy(x => x.Substring(0, 1));
+
+            //foreach (var item in query)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+
+            //// Output: fox the brown jumps quick
+
+            /*
+                2. Secondary Ascending Sort
+             */
+
+            string[] words = { "the", "quick", "brown", "fox", "jumps" };
+
+            //var query =
+            //    from word in words
+            //    orderby word.Length, word.Substring(0, 1) descending
+            //    select word;
+
+            var query =
+                words.OrderBy(x => x.Length).ThenByDescending(x => x.Substring(0, 1));
+
+            foreach (var item in query)
+            {
+                Console.Write($"{item} ");
+            }
+
+            // Output: the fox quick jumps brown
+
+        }
+    }
+}
